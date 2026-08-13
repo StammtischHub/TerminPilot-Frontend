6f339e8d523cde1f5bf2f3af1e6ed535d14b7906
leonreusch <leon@reusch-home.de>
2026-08-13 11:27:07 +0200

    import Temporal

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { theme } from './theme.ts';
 import { BrowserRouter } from 'react-router';
 import { AuthProvider } from './auth/AuthProvider.tsx';
+import 'temporal-polyfill/global';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

24c0e70474d607030dabf839a3dc0b508f8df62b
Leon Reusch <leon@reusch-home.de>
2026-08-23 21:39:07 +0200

    chore: run formatter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,14 @@
 import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import App from './App';
-import { MemoryRouter } from "react-router";
+import { MemoryRouter } from 'react-router';
 
 describe('App', () => {
   it('rendert ohne Fehler', () => {
     render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     expect(document.body).toBeInTheDocument();
   });

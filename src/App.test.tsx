757864543e50dd02a34bf436259ad464477a4c4b
Leon Reusch <leon@reusch-home.de>
2026-08-23 21:35:26 +0200

    test: fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import App from './App';
+import { MemoryRouter } from "react-router";
 
 describe('App', () => {
   it('rendert ohne Fehler', () => {
-    render(<App />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(document.body).toBeInTheDocument();
   });
 });
